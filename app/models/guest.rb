f153af534ba291c6ccd5c1ceb0a03fe6fc28499b
Tr1ckC0 <trick.co@yahoo.com>
2020-04-12 23:02:29 -0500

    submission for deadline

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -5,4 +5,8 @@ class Guest < ApplicationRecord
     def sort_episodes_by_rating
         self.episodes.sort_by { |episode| episode.average_rating }.reverse
     end
+
+    def rating_by_episode(episode)
+        Appearance.find_by(guest: self, episode: episode).rating
+      end
 end
